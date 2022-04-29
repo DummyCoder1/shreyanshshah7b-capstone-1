4cbbba155c9649259fca2fb2501a97d7fb25607c
DummyCoder1 <100186733+DummyCoder1@users.noreply.github.com>
2022-04-29 08:18:57 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,21 @@
+class Box {
+  
+    constructor(x,y,w,h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+
+    //make the color boxes appear on canvas
+    appear()
+
+
+    {
+      stroke(10);
+      strokeWeight(3);
+      fill("white");
+      rect(this.x,this.y,this.w,this.h);
+    }
+}
+
